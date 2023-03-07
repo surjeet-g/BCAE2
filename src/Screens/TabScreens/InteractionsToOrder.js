4a311813870a09acc05d9a45ac10ae8fd756640c
BCT-Kamal <mudaliar.k@bahwancybertek.com>
2023-03-07 20:11:54 +0530

    Interactions modal

--- src/Screens/TabScreens/InteractionsToOrder.js
+++ src/Screens/TabScreens/InteractionsToOrder.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from "react";
-import { Dimensions, ImageBackground, StyleSheet, View } from "react-native";
+import React, { useEffect, useRef, useMemo, useCallback } from "react";
+import {
+  Dimensions,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+  Image,
+  Text,
+} from "react-native";
 
-import { Text, useTheme } from "react-native-paper";
+import { useTheme, Switch } from "react-native-paper";
 
 import { useDispatch, useSelector } from "react-redux";
 import BCAE_LOGO from "../../Assets/svg/bcae_logo.svg";
@@ -15,8 +22,197 @@ import { changeLanguage } from "../../Utilities/Language/MulitLanguageSupport";
 import { getLanguage } from "../../Utilities/Language/language";
 var { height, width } = Dimensions.get("screen");
 
+import BottomSheet from "@gorhom/bottom-sheet";
+import { ICON_STYLE } from "./../../Utilities/Style/navBar";
+import { CustomDropDown } from "./../../Components/CustomDropDown";
+import { CustomInput } from "./../../Components/CustomInput";
+
 const InteractionsToOrder = ({ route, navigation }) => {
-  return <View style={{ flex: 1 }}></View>;
+  // ref
+  const interactionsModalRef = useRef(BottomSheet);
+  // variables
+  const interactionsModalSnapPoints = useMemo(() => ["10%", "70%"], []);
+  // callbacks
+  const handleSheetChanges = useCallback((index) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  const [isEnableInteractions, setIsEnableInteractions] = React.useState(false);
+  const onToggleInteractionSwitch = () =>
+    setIsEnableInteractions(!isEnableInteractions);
+
+  const InteractionsModalView = () => {
+    return (
+      <View style={styles.modalContainer}>
+        {/* ENable Interactions with Close */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginVertical: 10,
+            marginHorizontal: 15,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: 600,
+              color: "#202223",
+              fontSize: 16,
+              flex: 1,
+            }}
+          >
+            {"Enable Interactions"}
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Image
+              style={{ ...ICON_STYLE, color: "#36393D" }}
+              source={require("../../Assets/icons/ic_close.png")}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* ENable Interactions with Switch */}
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginVertical: 10,
+            marginHorizontal: 15,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: 600,
+              color: "#202223",
+              fontSize: 16,
+              flex: 1,
+            }}
+          >
+            {"Enable Interactions"}
+          </Text>
+          <Switch
+            value={isEnableInteractions}
+            onValueChange={onToggleInteractionSwitch}
+          />
+        </View>
+
+        {/* Field View */}
+        <View style={{ marginHorizontal: 10 }}>
+          <CustomDropDown
+            selectedValue={""}
+            setValue={""}
+            data={[]}
+            onChangeText={(text) => console.log(text)}
+            value={""}
+            placeHolder={strings.intractionType}
+          />
+          <CustomDropDown
+            selectedValue={""}
+            setValue={""}
+            data={[]}
+            onChangeText={(text) => console.log(text)}
+            value={""}
+            placeHolder={strings.serviceType}
+          />
+          <CustomDropDown
+            selectedValue={""}
+            setValue={""}
+            data={[]}
+            onChangeText={(text) => console.log(text)}
+            value={""}
+            placeHolder={strings.problem_stat_cause}
+          />
+          <CustomDropDown
+            selectedValue={""}
+            setValue={""}
+            data={[]}
+            onChangeText={(text) => console.log(text)}
+            value={""}
+            placeHolder={strings.priority_type}
+          />
+          <CustomDropDown
+            selectedValue={""}
+            setValue={""}
+            data={[]}
+            onChangeText={(text) => console.log(text)}
+            value={""}
+            placeHolder={strings.contact_type}
+          />
+          <CustomInput
+            value={""}
+            caption={strings.remarks}
+            placeHolder={strings.remarks}
+            onChangeText={(text) => console.log(text)}
+          />
+          <CustomInput
+            value={""}
+            caption={strings.attachment}
+            placeHolder={strings.attachment}
+            onChangeText={(text) => console.log(text)}
+          />
+
+          <CustomInput
+            value={""}
+            caption={strings.remarks}
+            placeHolder={strings.remarks}
+            onChangeText={(text) => console.log(text)}
+          />
+          <CustomInput
+            value={""}
+            caption={strings.attachment}
+            placeHolder={strings.attachment}
+            onChangeText={(text) => console.log(text)}
+          />
+
+          <CustomInput
+            value={""}
+            caption={strings.remarks}
+            placeHolder={strings.remarks}
+            onChangeText={(text) => console.log(text)}
+          />
+          <CustomInput
+            value={""}
+            caption={strings.attachment}
+            placeHolder={strings.attachment}
+            onChangeText={(text) => console.log(text)}
+          />
+        </View>
+
+        {/* Bottom Button View */}
+        <View
+          style={{
+            flexDirection: "row",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+            backgroundColor: "white",
+            marginHorizontal: 15,
+          }}
+        >
+          <View style={{ flex: 1 }}>
+            <CustomButton label={strings.cancel} onPress={() => {}} />
+          </View>
+          <View style={{ flex: 1 }}>
+            <CustomButton label={strings.submit} onPress={() => {}} />
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <BottomSheet
+        ref={interactionsModalRef}
+        index={1}
+        snapPoints={interactionsModalSnapPoints}
+        onChange={handleSheetChanges}
+      >
+        <InteractionsModalView />
+      </BottomSheet>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
@@ -25,6 +221,10 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+  },
+  modalContainer: {
+    flex: 1,
+    padding: 10,
   },
   triangleCorner: {
     width: 0,
