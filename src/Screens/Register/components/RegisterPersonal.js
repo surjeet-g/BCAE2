0a3b8b7f70c589b9423555abcc9f6ede8afba87c
v0647 <vipin.v@bahwancybertek.com>
2023-03-15 17:14:34 +0530

    register

--- src/Screens/Register/components/RegisterPersonal.js
+++ src/Screens/Register/components/RegisterPersonal.js
@@ -227,13 +227,13 @@ export const RegisterPersonal = React.memo(({ navigation }) => {
     //   });
     //   return null;
     // }
-    // if (!emailOTPVerification) {
-    //   Toast.show({
-    //     type: "bctError",
-    //     text1: strings.otpErrorMsgForEmail,
-    //   });
-    //   return null;
-    // }
+    if (!emailOTPVerification) {
+      Toast.show({
+        type: "bctError",
+        text1: strings.otpErrorMsgForEmail,
+      });
+      return null;
+    }
 
     if (!validatePassword(password)) {
       setPasswordError(strings.passwordValidError);
