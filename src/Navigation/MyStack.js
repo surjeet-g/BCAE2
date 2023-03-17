2fbec4c8c4f7c2a8a28ca7a317675c6b7df418f1
BCT-Kamal <mudaliar.k@bahwancybertek.com>
2023-03-17 16:44:20 +0530

    Add more icon

--- src/Navigation/MyStack.js
+++ src/Navigation/MyStack.js
@@ -357,9 +357,27 @@ function MyStack() {
         />
         <Stack.Screen
           options={({ navigation }) => ({
-            ...options,
             ...{
+              headerTintColor: "#fff",
               headerTitle: "Interaction Details",
+              headerBackgroundContainerStyle: { backgroundColor: "#4C5A81" },
+              headerTitleStyle: {
+                ...fonts.titleLarge,
+                ...{ color: "#fff", fontWeight: "700" },
+              },
+              headerRight: () => {
+                return (
+                  <View style={{ marginRight: 15 }}>
+                    <Pressable
+                      onPress={() => navigation.navigate("EditProfile")}
+                    >
+                      <Image
+                        source={require("../Assets/icons/ic_more_vertical.png")}
+                      />
+                    </Pressable>
+                  </View>
+                );
+              },
             },
           })}
           name={STACK_INTERACTION_DETAILS}
@@ -367,9 +385,27 @@ function MyStack() {
         />
         <Stack.Screen
           options={({ navigation }) => ({
-            ...options,
             ...{
+              headerTintColor: "#fff",
               headerTitle: "Interaction Details",
+              headerBackgroundContainerStyle: { backgroundColor: "#4C5A81" },
+              headerTitleStyle: {
+                ...fonts.titleLarge,
+                ...{ color: "#fff", fontWeight: "700" },
+              },
+              headerRight: () => {
+                return (
+                  <View style={{ marginRight: 15 }}>
+                    <Pressable
+                      onPress={() => navigation.navigate("EditProfile")}
+                    >
+                      <Image
+                        source={require("../Assets/icons/ic_more_vertical.png")}
+                      />
+                    </Pressable>
+                  </View>
+                );
+              },
             },
           })}
           name={STACK_FOLLOWUP}
--- src/Screens/Appointments/InteractionDetails.js
+++ src/Screens/Appointments/InteractionDetails.js
@@ -384,6 +384,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#F0F0F0",
+    paddingTop: 50,
   },
   scrollviewContainer: {
     margin: 15,
