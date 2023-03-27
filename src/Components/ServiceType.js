b7a47bf9f6eb09bd8593bb4f3f17bb0267eae5ac
BCT-Kamal <mudaliar.k@bahwancybertek.com>
2023-03-27 14:33:49 +0530

    Service type component

---
+++ src/Components/ServiceType.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, Pressable } from "react-native";
+import React, { useState } from "react";
+
+const ServiceType = (props) => {
+  const { name = "NA" } = props;
+  const [selected, setSelected] = useState(false);
+  return (
+    <Pressable
+      style={{
+        ...styles.container,
+        backgroundColor: selected ? "#4C5A81" : "white",
+        borderColor: selected ? "#4C5A81" : "#AEADB0",
+      }}
+      onPress={() => setSelected(!selected)}
+    >
+      <Text
+        style={{ ...styles.nameTxt, color: selected ? "white" : "#2B2B2B" }}
+      >
+        {name}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default ServiceType;
+
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "#AEADB0",
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    alignSelf: "center",
+  },
+  nameTxt: {
+    color: "#2B2B2B",
+    fontWeight: 400,
+    fontSize: 14,
+  },
+});
