656d9efcce707f00cdc021a891a7164d5ea61ef2
v0647 <vipin.v@bahwancybertek.com>
2023-05-23 16:58:17 +0530

    login

--- src/Screens/Login/Login.js
+++ src/Screens/Login/Login.js
@@ -63,13 +63,13 @@ export const Login = ({ navigation }) => {
   const [loginMode, setLoginMode] = useState(EMAIL); // EMAIL or MOBILE
   const [loginType, setLoginType] = useState(PASSWORD); // PASSWORD or OTP
   const [isFirstSelected, setFirstSelected] = useState(false);
-  const [username, setUsername] = useState("aher.dipak@bahwancybertek.com");
-  const [password, setPassword] = useState("Test@123");
+  // const [username, setUsername] = useState("aher.dipak@bahwancybertek.com");
+  // const [password, setPassword] = useState("Test@123");
   //bussiness
   // const [username, setUsername] = useState("mobappbcae@yopmail.com");
   // const [password, setPassword] = useState("Test@123");
-  // const [username, setUsername] = useState("");
-  // const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const [usernameError, setUsernameError] = useState("");
   const [passwordError, setPasswordError] = useState("");
   const [secureTextEntry, setsecureTextEntry] = useState(true);
