41ab2247587464d7aa56bf7ff5efb2f36c72c224
v0647 <vipin.v@bahwancybertek.com>
2023-07-17 15:16:21 +0530

    add gredeint

--- src/Components/FooterModel.js
+++ src/Components/FooterModel.js
@@ -4,11 +4,11 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { SHADOW_STYLE } from "../Utilities/themeConfig";
 import { ClearSpace } from "./ClearSpace";
-Icon;
+
 var { height, width } = Dimensions.get("screen");
 
 export const FooterModel = ({
-  setOpen = () => {},
+  setOpen = () => { },
   open = false,
   children,
   title = "",
@@ -16,107 +16,113 @@ export const FooterModel = ({
 }) => {
   if (!open) return null;
   return (
-    <View
-      style={{
-        position: "absolute",
-        bottom: 1,
-        width: width,
-        backgroundColor: "transparent",
-      }}
-    >
-      {/* header */}
+    <>
+      <View style={{ backgroundColor: "#928d8dab", height: height, width: width, position: "absolute" }} />
+
+
       <View
         style={{
-          backgroundColor: "white",
-          minHeight: height * 0.2,
-          maxHeight: height * 0.8,
-          borderTopLeftRadius: 15,
-          borderTopRightRadius: 15,
-          ...SHADOW_STYLE,
+          position: "absolute",
+          bottom: 1,
+          width: width,
+          backgroundColor: "transparent",
         }}
       >
+        {/* header */}
         <View
           style={{
-            position: "relative",
-            top: 1,
-
-            justifyContent: "center",
-            alignItems: "center",
+            backgroundColor: "white",
+            minHeight: height * 0.2,
+            maxHeight: height * 0.8,
+            borderTopLeftRadius: 15,
+            borderTopRightRadius: 15,
+            ...SHADOW_STYLE,
           }}
         >
-          <ClearSpace size={3} />
           <View
             style={{
-              width: 70,
-              height: 3,
-              backgroundColor: "black",
-              borderRadius: 5,
-            }}
-          />
-        </View>
+              position: "relative",
+              top: 1,
 
-        <ClearSpace size={2} />
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            // position: "relative",
-            marginVertical: 5,
-            // flex: 1,
-            // height: 300,
-            marginHorizontal: 15,
-          }}
-        >
-          <Text
-            style={{
-              fontWeight: 600,
-              color: "#202223",
-              fontSize: 16,
-              // flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            {title}
-          </Text>
+            <ClearSpace size={3} />
+            <View
+              style={{
+                width: 70,
+                height: 3,
+                backgroundColor: "black",
+                borderRadius: 5,
+              }}
+            />
+          </View>
 
-          <Icon
-            onPress={() => {
-              setOpen(false);
-            }}
-            name="close-circle"
-            size={25}
-            color={"#000"}
-          />
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            // position: "relative",
-            marginVertical: 5,
-            // flex: 1,
-            // height: 300,
-            marginHorizontal: 15,
-          }}
-        >
-          <Text
+          <ClearSpace size={2} />
+          <View
             style={{
-              fontWeight: 400,
-              color: "#3D3D3D",
-              fontSize: 16,
+              flexDirection: "row",
+              justifyContent: "space-between",
+              // position: "relative",
+              marginVertical: 5,
               // flex: 1,
+              // height: 300,
+              marginHorizontal: 15,
             }}
           >
-            {subtitle}
-          </Text>
-        </View>
+            <Text
+              style={{
+                fontWeight: 600,
+                color: "#202223",
+                fontSize: 16,
+                // flex: 1,
+              }}
+            >
+              {title}
+            </Text>
 
-        <ScrollView
-          nestedScrollEnabled={true}
-          contentContainerStyle={{ marginBottom: 20 }}
-        >
-          {children}
-        </ScrollView>
+            <Icon
+              onPress={() => {
+                setOpen(false);
+              }}
+              name="close-circle"
+              size={25}
+              color={"#000"}
+            />
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              // position: "relative",
+              marginVertical: 5,
+              // flex: 1,
+              // height: 300,
+              marginHorizontal: 15,
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: 400,
+                color: "#3D3D3D",
+                fontSize: 16,
+                // flex: 1,
+              }}
+            >
+              {subtitle}
+            </Text>
+          </View>
+
+          <ScrollView
+            nestedScrollEnabled={true}
+            contentContainerStyle={{ marginBottom: 20 }}
+          >
+            {children}
+          </ScrollView>
+        </View>
       </View>
-    </View>
+    </>
+
   );
 };
