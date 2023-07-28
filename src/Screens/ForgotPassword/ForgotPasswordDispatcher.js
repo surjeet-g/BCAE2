d89ddc147b1e74cd78b7d2f90ae0bf5c6de4e670
v0647 <vipin.v@bahwancybertek.com>
2023-07-28 12:32:19 +0530

    r

--- src/Screens/ForgotPassword/ForgotPasswordDispatcher.js
+++ src/Screens/ForgotPassword/ForgotPasswordDispatcher.js
@@ -1,15 +1,19 @@
-import {
-  initForgotPasswordData,
-  failureForgotPassword,
-  setForgotPasswordData,
-  resetForgotPasswordData,
-} from "./ForgotPasswordAction";
+import Toast from "react-native-toast-message";
 import { serverCall } from "../../Utilities/API";
 import { endPoints, requestMethod } from "../../Utilities/API/ApiConstants";
-import { saveToken, getToken } from "../../Storage/token";
-import { passwordHash } from "../../Utilities/Constants/Constant";
-import Toast from "react-native-toast-message";
+import {
+  failureForgotPassword, initForgotPasswordData, resetForgotPasswordData, setForgotPasswordData
+} from "./ForgotPasswordAction";
 
+
+/**
+* Reducer Dispatch
+* Handle API call for verify forgot password API
+* @memberOf ForgotPassword
+* @param  {Object} params payload for API
+* @param  {Object} navigation handle section timeout
+* @returns {Object} Dispatcher to reducer
+*/
 export function verifyForgotPasswordData(navigation, params, type) {
   return async (dispatch) => {
     dispatch(initForgotPasswordData());
@@ -46,13 +50,28 @@ export function verifyForgotPasswordData(navigation, params, type) {
     }
   };
 }
-
+/**
+* Reducer Dispatch
+* Handle reset form data
+* @memberOf ForgotPassword
+* @returns {Object} Dispatcher to reducer
+*/
 export function resetForgotPassword() {
   return async (dispatch) => {
     dispatch(resetForgotPasswordData());
   };
 }
 
+/**
+* Reducer Dispatch
+* Handle API call for change password
+* @memberOf ForgotPassword
+* @param  {string} email email address
+* @param  {string} oldPassword old password 
+* @param  {string} newPassword new password 
+* @param  {string} confirmPassword confirm password
+* @returns {Object} Dispatcher to reducer
+*/
 export function changePassword(
   email,
   oldPassword,
